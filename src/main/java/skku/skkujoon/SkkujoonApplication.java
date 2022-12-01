ec240fa1c4c19ec32fefe3dcec66db9f3fa28db8
Seungil Kim <ksi990302@gmail.com>
2022-12-01 15:18:15 +0900

    Initial commit

---
+++ src/main/java/skku/skkujoon/SkkujoonApplication.java
@@ -0,0 +1,13 @@
+package skku.skkujoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkkujoonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkkujoonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/skku/skkujoon/SkkujoonApplicationTests.java
@@ -0,0 +1,13 @@
+package skku.skkujoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkkujoonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
