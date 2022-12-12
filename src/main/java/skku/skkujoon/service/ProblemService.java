c19b8c6e1155365eb6b9709de407a8fbe9d0fb44
Seungil Kim <ksi990302@gmail.com>
2022-12-12 13:46:07 +0900

    update service (solve problem)

--- src/main/java/skku/skkujoon/service/ProblemService.java
+++ src/main/java/skku/skkujoon/service/ProblemService.java
@@ -6,6 +6,8 @@ import org.springframework.transaction.annotation.Transactional;
 import skku.skkujoon.domain.Problem;
 import skku.skkujoon.repository.ProblemRepository;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -13,9 +15,25 @@ public class ProblemService {
 
     private final ProblemRepository problemRepository;
 
-
     public boolean isDuplicate(Problem problem) {
         return problemRepository.findByProblemNumber(problem.getProblemNumber()).isPresent();
+    }
+
+    @Transactional
+    public void addProblem(Problem problem) {
+        problemRepository.save(problem);
+    }
+
+    public Problem findProblemById(Long problemId) {
+        return problemRepository.findById(problemId).orElseThrow(IllegalArgumentException::new);
+    }
+
+    public Problem findProblemByProblemNumber(Long problemNumber) {
+        return problemRepository.findByProblemNumber(problemNumber).orElseThrow(IllegalArgumentException::new);
+    }
+
+    public List<Problem> findProblems() {
+        return problemRepository.findAll();
     }
 
 }
--- src/main/java/skku/skkujoon/service/UserService.java
+++ src/main/java/skku/skkujoon/service/UserService.java
@@ -3,10 +3,13 @@ package skku.skkujoon.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import skku.skkujoon.domain.Problem;
 import skku.skkujoon.domain.User;
+import skku.skkujoon.domain.UserProblem;
+import skku.skkujoon.repository.ProblemRepository;
 import skku.skkujoon.repository.UserRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,9 +17,37 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final ProblemRepository problemRepository;
 
-    public boolean isDuplicate(User user) {
-       return userRepository.findByHandle(user.getHandle()).isPresent();
+    public boolean isDuplicate(String handle) {
+        return userRepository.findByHandle(handle).isPresent();
+    }
+
+    @Transactional
+    public void addUser(User user) {
+        userRepository.save(user);
+    }
+
+    public User findUserById(Long userId) {
+        return userRepository.findById(userId).orElseThrow(IllegalArgumentException::new);
+    }
+
+    public User findUserByHandle(String handle) {
+        return userRepository.findByHandle(handle).orElseThrow(IllegalAccessError::new);
+    }
+
+    public List<User> findUsers() {
+        return userRepository.findAll();
+    }
+
+    @Transactional
+    public void solveProblem(Long userId, Long problemId) {
+        UserProblem userProblem = new UserProblem();
+        User user = userRepository.findById(userId).orElseThrow(IllegalArgumentException::new);
+        Problem problem = problemRepository.findById(problemId).orElseThrow(IllegalArgumentException::new);
+
+        user.addUserProblem(userProblem);
+        problem.addUserProblem(userProblem);
     }
 
 }
