808d12164b9e646a6634000637efc6228fe64d5e
Seungil Kim <ksi990302@gmail.com>
2022-12-13 15:54:11 +0900

    add scheduler

---
+++ src/main/java/skku/skkujoon/Scheduler.java
@@ -0,0 +1,74 @@
+package skku.skkujoon;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import skku.skkujoon.domain.Problem;
+import skku.skkujoon.domain.User;
+import skku.skkujoon.domain.dto.UpdateProblemDto;
+import skku.skkujoon.domain.dto.UpdateUserDto;
+import skku.skkujoon.service.ProblemService;
+import skku.skkujoon.service.UserService;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class Scheduler {
+
+    private final UserService userService;
+    private final ProblemService problemService;
+    private final DataLoader dataLoader;
+
+    @Transactional
+    public void task() {
+
+        List<Problem> problemList = dataLoader.getAllProblemList();
+
+        for (Problem p : problemList) {
+            Optional<Problem> optionalProblem = problemService.findByProblemNumber(p.getProblemNumber());
+            if (optionalProblem.isPresent()) {
+                Problem problem = optionalProblem.get();
+                UpdateProblemDto dto = new UpdateProblemDto(
+                        p.getTitleKo(),
+                        p.getLevel(),
+                        p.isSolvable(),
+                        p.isPartial()
+                );
+                problem.updateProblem(dto);
+            } else {
+                problemService.addProblem(p);
+            }
+        }
+
+        List<User> userList = dataLoader.getUserList();
+
+        for (User u : userList) {
+            Optional<User> optionalUser = userService.findByHandle(u.getHandle());
+            if (optionalUser.isPresent()) {
+                User user = optionalUser.get();
+                UpdateUserDto dto = new UpdateUserDto(
+                        u.getBio(),
+                        u.getSolvedCount(),
+                        u.getTier(),
+                        u.getRating(),
+                        u.getRanking(),
+                        u.getGlobalRank());
+                user.updateUser(dto);
+            } else {
+                userService.addUser(u);
+            }
+        }
+
+        for (User u : userList) {
+            List<Problem> userProblemList = dataLoader.getUserProblemList(u.getHandle());
+            for (Problem p : userProblemList) {
+                userService.solveProblem(u.getId(), p.getId());
+            }
+        }
+    }
+
+}
