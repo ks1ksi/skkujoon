a15beb615a2464f524e45b2564c4bd3b07f0450b
Seungil Kim <ksi990302@gmail.com>
2022-12-13 18:37:26 +0900

    remove import

--- src/main/java/skku/skkujoon/aop/DelayAop.java
+++ src/main/java/skku/skkujoon/aop/DelayAop.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.stereotype.Component;
 
 @Aspect
 @Slf4j
