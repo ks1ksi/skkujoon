fe41c6a016c55392688828d2c3aa9789625e13af
Seungil Kim <ksi990302@gmail.com>
2023-01-16 21:08:50 +0900

    add random api

--- src/main/java/skku/skkujoon/controller/api/ProblemApiController.java
+++ src/main/java/skku/skkujoon/controller/api/ProblemApiController.java
@@ -2,6 +2,7 @@ package skku.skkujoon.controller.api;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import skku.skkujoon.domain.Problem;
 import skku.skkujoon.domain.dto.ProblemDto;
@@ -19,6 +20,14 @@ public class ProblemApiController {
     @GetMapping("/api/v1/problems/unsolved")
     public List<ProblemDto> unsolvedProblems() {
         return problemService.findUnsolvedProblems().stream().map(
+                p -> new ProblemDto(p.getProblemNumber(), p.getTitleKo(), p.getLevel(), p.getSolvedBySkku())
+        ).collect(Collectors.toList());
+    }
+
+    @GetMapping("/api/v1/problems/unsolved/random")
+    public List<ProblemDto> randomUnsolvedProblems(@RequestParam int limit) {
+        List<Problem> randomUnsolvedProblems = problemService.findRandomUnsolvedProblems(limit);
+        return randomUnsolvedProblems.stream().map(
                 p -> new ProblemDto(p.getProblemNumber(), p.getTitleKo(), p.getLevel(), p.getSolvedBySkku())
         ).collect(Collectors.toList());
     }
